6d374e552dfbcfe57f38a1a87db41ac136265a39
Hiroki Honda <hon87kot12.xp@gmail.com>
2017-06-09 01:06:26 -0700

    Kotlin最適化

--- AndroidKotlinPlayground/app/src/main/java/com/example/hiroki/androidkotlinplayground/MainActivity.kt
+++ AndroidKotlinPlayground/app/src/main/java/com/example/hiroki/androidkotlinplayground/MainActivity.kt
@@ -16,65 +16,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView = findViewById(R.id.list) as ListView
-        listView.adapter = CustomAdapter()
+        (findViewById(R.id.list) as ListView).adapter = CustomAdapter()
     }
 
     private inner class CustomAdapter : BaseAdapter() {
 
         internal val list: MutableList<RowData> = ArrayList()
 
-        override fun getCount(): Int {
-            return 50
-        }
+        override fun getCount(): Int = 50
 
-        override fun getItem(position: Int): RowData {
-            if (list.size > position) {
-                return list[position]
-            } else {
-                val newRowData = RowData(position)
-                list.add(newRowData)
-                return newRowData
-            }
-        }
+        override fun getItem(position: Int): RowData =
+                if (list.size > position) {
+                    list[position]
+                } else {
+                    val newRowData = RowData(position)
+                    list.add(newRowData)
+                    newRowData
+                }
 
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
+        override fun getItemId(position: Int): Long = position.toLong()
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-            var convertView = convertView
-            if (convertView == null) {
-                convertView = layoutInflater
-                        .inflate(android.R.layout.simple_list_item_2, null)
-            }
+            val view = convertView ?: layoutInflater.inflate(android.R.layout.simple_list_item_2, null)
 
             // get row data
             val rowData = getItem(position)
             rowData.increment()
 
-            // set title
-            val titleView = convertView!!.findViewById(android.R.id.text1) as TextView
-            titleView.text = rowData.getTitle()
+            // view set up
+            view.apply {
+                // set title
+                (findViewById(android.R.id.text1) as TextView).text = rowData.getTitle()
 
-            // set sub-title
-            val summaryView = convertView.findViewById(android.R.id.text2) as TextView
-            val summary = rowData.summary
-            if (summary == null) {
-                summaryView.visibility = View.GONE
-            } else {
-                summaryView.visibility = View.VISIBLE
-                summaryView.text = rowData.summary
+                // set sub-title
+                (findViewById(android.R.id.text2) as TextView).apply {
+                    visibility = rowData.summary?.let { View.VISIBLE } ?: View.GONE
+                    text = rowData.summary?.let { it }
+                }
+
+                // set background color
+                setBackgroundColor(ContextCompat.getColor(applicationContext,
+                        if (position % 2 == 0) android.R.color.white else android.R.color.holo_green_light))
             }
 
-            // set background color
-            convertView.setBackgroundColor(
-                    if (position % 2 == 0)
-                        ContextCompat.getColor(applicationContext, android.R.color.white)
-                    else
-                        ContextCompat.getColor(applicationContext, android.R.color.holo_green_light))
-
-            return convertView
+            return view
         }
     }
 }
--- AndroidKotlinPlayground/app/src/main/java/com/example/hiroki/androidkotlinplayground/RowData.kt
+++ AndroidKotlinPlayground/app/src/main/java/com/example/hiroki/androidkotlinplayground/RowData.kt
@@ -1,27 +1,16 @@
 package com.example.hiroki.androidkotlinplayground
 
-import java.util.Random
+import java.util.*
 
-class RowData internal constructor(val row: Int) {
-    private val title: String
-    val summary: String?
+class RowData internal constructor(private val row: Int) {
+
+    val summary: String? =
+            if (Random().nextInt(5) == 0) null
+            else "Summary " + row
+
     private var count = 0
 
-    init {
-        this.title = "Row " + (row + 1)
-        val random = Random().nextInt(5)
-        if (random % 5 == 0) {
-            this.summary = null
-        } else {
-            this.summary = "Summary " + row
-        }
-    }
+    fun getTitle(): String = "Row " + (row + 1) + " count " + count
 
-    fun getTitle(): String {
-        return title + " count " + count
-    }
-
-    fun increment() {
-        count++
-    }
+    fun increment() = count++
 }
